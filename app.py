b6ff300f4a554310966209e920cd787a460c5fb6
victoriasands <vsands@teprmail.umd.edu>
2024-02-19 20:55:53 -0500

    needed to update app.py to route the app

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ def hello_world():  # put application's code here
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template('hello.html')
-
+@app.route('about')
+def about():  # put application's code here
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
