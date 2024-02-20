df6c7d99f5572e97e5bbe4532b29f92afdf6bd55
victoriasands <vsands@teprmail.umd.edu>
2024-02-19 20:59:45 -0500

    trying to update app.py to create the /about page

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def hello_world():  # put application's code here
 def hello():  # put application's code here
     return render_template('hello.html')
 @app.route('/about')
-def about():  # put application's code here
-    return render_template('about.html')
+def about():
+    return render_template(about.html)
 
 if __name__ == '__main__':
     app.run()
