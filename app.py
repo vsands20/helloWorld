4f66e173f8701268b85fbb04e7e44571cfc77e6d
victoriasands <vsands@teprmail.umd.edu>
2024-03-05 17:48:19 -0500

    committing the app.py update for about-css

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def hello():  # put application's code here
 def about():
     return render_template('about.html')
 
+@app.route('/about-css')
+def aboutcss():
+    return render_template('about-css.html')
+
 if __name__ == '__main__':
     app.run()
